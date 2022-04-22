46fc884f79d825a6bdb269ea895d55f4ebe0ed37
Julien SOSTHENE <tiesse@pm.me>
2022-04-22 12:11:39 +0200

    Added initial Woof source code

---
+++ pwa/woof/assets/main.js
@@ -0,0 +1,23 @@
+const url = "https://dog.ceo/api/breeds/image/random";
+const img = document.querySelector("#picture");
+const button = document.querySelector("#randomizer");
+
+const getPicture = async () => {
+    try {
+        const response = await fetch(url);
+        const json = await response.json();
+        if(json.status === "success"){
+            img.src = json.message;
+        }
+        else {
+            console.error("API responded with error code.");
+        }
+    }
+    catch(error){
+        console.error("Could not fetch dog picture from API",error);
+    }
+}
+
+getPicture();
+
+button.addEventListener("click", getPicture);
