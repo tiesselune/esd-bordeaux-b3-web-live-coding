e0747029e56b44482bed96fd4f26b08910abae40
Julien SOSTHENE <tiesse@pm.me>
2022-06-03 09:14:05 +0200

    Added strikethrough

--- react-native/todo-list/components/Todo.js
+++ react-native/todo-list/components/Todo.js
@@ -1,11 +1,18 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import CheckBox from "react-native-check-box";
 
 export default ({text,done,onToggle}) => {
     
     return (
-        <View>
-            <Text><CheckBox isChecked={done} onClick={onToggle}/><Text>{text}</Text></Text>
+        <View >
+            <View style={styles.todo}><CheckBox isChecked={done} onClick={onToggle} checkBoxColor="#34d38e"/><Text style={{textDecorationLine : (done ? "line-through" : "none"), color : "black"}}>{text}</Text></View>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    todo : {
+        flexDirection : "row",
+        alignItems : "center"
+    }
+})
