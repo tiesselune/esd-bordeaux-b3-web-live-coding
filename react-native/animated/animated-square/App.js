3c2dd91e0dede41e687aa5583ea3b7fec9c36151
Julien SOSTHENE <tiesse@pm.me>
2022-06-03 10:33:25 +0200

    Updated animated with press in / press out

--- react-native/animated/animated-square/App.js
+++ react-native/animated/animated-square/App.js
@@ -6,22 +6,22 @@ export default function App() {
   const value = useRef(new Animated.Value(0)).current;
   const radius = value.interpolate({
     inputRange : [0,1],
-    outputRange : [0,25]
+    outputRange : [0,50]
   });
   const color = value.interpolate({
     inputRange : [0,1],
     outputRange : ["rgb(5,5,200)","rgb(170,0,170)"]
   });
-  const launchAnimation = () => {
-      Animated.sequence([
-        Animated.timing(value,{toValue : 1, duration : 1000, useNativeDriver : false}),
-        Animated.timing(value,{toValue : 0, duration : 1000, useNativeDriver : false})
-      ]).start();
+  const launchAnimationIn = () => {
+        Animated.spring(value,{toValue : 1,friction : 3,tension : 20, useNativeDriver : false}).start();
   };
+  const launchAnimationOut = () => {
+      Animated.spring(value,{toValue : 0,friction : 5,tension : 40, useNativeDriver : false}).start();
+};
   return (
     <View style={styles.container}>
       <Animated.View style={{...styles.square, borderRadius :radius, backgroundColor : color}}>
-        <Pressable style={{width : 100, height : 100}} onPress={launchAnimation}></Pressable>
+        <Pressable android_ripple={{color : "white"}} style={{width : 200, height : 200}} onPressIn={launchAnimationIn} onPressOut={launchAnimationOut}></Pressable>
       </Animated.View>
       <StatusBar style="auto" />
     </View>
@@ -36,8 +36,9 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   square : {
-    width : 100,
-    height : 100,
+    width : 200,
+    height : 200,
     backgroundColor : "blue",
+    overflow : "hidden",
   }
 });
