136950808aee7abade05a3dcfec8c4115f99affe
Julien SOSTHENE <tiesse@pm.me>
2022-06-03 10:55:49 +0200

    Updated todo with pressable

--- react-native/todo-list/components/Todo.js
+++ react-native/todo-list/components/Todo.js
@@ -1,12 +1,12 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Pressable } from "react-native";
 import CheckBox from "react-native-check-box";
 
 export default ({text,done,onToggle}) => {
     
     return (
-        <View >
+        <Pressable style={styles.todo} onPress={onToggle}>
             <View style={styles.todo}><CheckBox isChecked={done} onClick={onToggle} checkBoxColor="#34d38e"/><Text style={{textDecorationLine : (done ? "line-through" : "none"), color : "black"}}>{text}</Text></View>
-        </View>
+        </Pressable>
     );
 };
 
